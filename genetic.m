e24b1e770296dd661c87b49de4f7fc305c801b5b
AlanXue1997 <836106158@qq.com>
2018-04-14 14:03:07 +0800

    common update

--- genetic.m
+++ genetic.m
@@ -1,4 +1,7 @@
 clear,clc;
 
-graph = getDAG();
-population = initPopulation();
+% graph = getDAG();
+% population = initPopulation(graph, m);
+
+a = [0 5 1 6 4 7 9 3 2 8];
+order = sortOrder(a);
--- initPopulation.m
+++ initPopulation.m
@@ -1,7 +1,7 @@
 function [ output_args ] = initPopulation( input_args )
 %INITPOPULATION Summary of this function goes here
 %   Detailed explanation goes here
-
+    output(i,:) = getPerson(G)
 
 end
 
