4059bd6104b15c467960992113265c6e8a1c9929
AlanXue1997 <836106158@qq.com>
2018-04-15 22:52:55 +0800

    solve bugs of decoding

--- genetic.m
+++ genetic.m
@@ -4,9 +4,11 @@ graph = getDAG('.\作业优先关系矩阵.xlsx');
 m = 10;
 population = initPopulation(graph,m);
 
-p = decoding(population(1,:), 3);
+p = decoding(population(1,:), 5);
 disp(getMSD(p));
+disp(p');
 
 for i=1:m
     disp(isTopology(population(i,:),graph));
 end
+
