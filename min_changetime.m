71da09c25a0b0583d94a47c25966a9f4b7d20ff0
TxT168 <1034971128@qq.com>
2018-05-04 18:25:53 +0800

    恩

--- min_changetime.m
+++ min_changetime.m
@@ -1,5 +1,5 @@
 function [ minchangetime ] = min_changetime(p)
-    changetime = xlsread('.\产品切换时间表.xlsx');
+    changetime = [0 13 12;15 0 16; 16 14 0];
     minchangetime = 0;
     for i=2:14
         if p(i) ~= p(i-1)
